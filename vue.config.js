45330b7c0c15fe9f4adf6e8bb37e5a00070060ab
Duarte Miranda <duarte.miranda@kognic.com>
2026-01-07 21:48:15 +0100

    asd

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/dnd/'  // 🔴 your repo name here
-    : '/'
+  publicPath: '/dnd/'
 })
